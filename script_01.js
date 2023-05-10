dac610ebf1a2c9bcff740aa35ebd66e9d281164c
Peter <schmidt.multimedia@gmail.com>
2023-05-10 10:46:10 +0200

    CF / ternary

--- script_01.js
+++ script_01.js
@@ -3,7 +3,7 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
-const ageJohn = 30;
+const ageJohn = 35;
 const ageMark = 30;
 
 // Deklaration 
@@ -40,11 +40,15 @@ console.log("------------------");
 // if(true)
 // if(false)
 // if(isJohnOlder)
-if(ageJohn > ageMark)
-{
-    console.log("John ist älter."); 
-} 
-else 
-{
-    console.log("John ist jünger.");
-}
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter."); 
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+/************  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
